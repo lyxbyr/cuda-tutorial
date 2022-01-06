c6079ba9bbb448b0b02f827a3bceb23777d3a002
lyxbyr <13021939199@163.com>
2022-01-06 21:05:42 +0800

    add 03add_kernel grid, block

--- 03add_kernel.cu
+++ 03add_kernel.cu
@@ -1,19 +1,24 @@
-#include <stdio.h>
-#include <assert.h>
+#include "stdio.h"
+#include "assert.h"
 
+#define BLOCKS 7
+#define THREADS 3
 #define ARRAY_SIZE 5
 const int ARRAY_BYTES = ARRAY_SIZE * sizeof(int);
 
 
+
 __global__ void addKernel(int* d_a, int* d_b, int* d_result) {
 
-  int idx = threadIdx.x;
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
   d_result[idx] = d_a[idx] + d_b[idx];
 }
 
 
 void onDevice(int* h_a, int* h_b, int* h_result) {
+  
   int *d_a, *d_b, *d_result;
+
   cudaMalloc((void**)&d_a, ARRAY_BYTES);
   cudaMalloc((void**)&d_b, ARRAY_BYTES);
   cudaMalloc((void**)&d_result, ARRAY_BYTES);
@@ -21,40 +26,36 @@ void onDevice(int* h_a, int* h_b, int* h_result) {
   cudaMemcpy(d_a, h_a, ARRAY_BYTES, cudaMemcpyHostToDevice);
   cudaMemcpy(d_b, h_b, ARRAY_BYTES, cudaMemcpyHostToDevice);
 
-  addKernel<<<1, ARRAY_SIZE>>>(d_a, d_b, d_result);
+  addKernel<<<BLOCKS, THREADS >>>(d_a, d_b, d_result);
 
   cudaMemcpy(h_result, d_result, ARRAY_BYTES, cudaMemcpyDeviceToHost);
 
   cudaFree(d_a);
   cudaFree(d_b);
   cudaFree(d_result);
-
-
 }
+
 
 
 
 void onHost() {
 
   int *h_a, *h_b, *h_result;
-
-  h_a = (int*)malloc(ARRAY_BYTES);
-  h_b = (int*)malloc(ARRAY_BYTES);
+  h_a      = (int*)malloc(ARRAY_BYTES);
+  h_b      = (int*)malloc(ARRAY_BYTES);
   h_result = (int*)malloc(ARRAY_BYTES);
 
-  //init
   for (int i = 0; i < ARRAY_SIZE; ++i) {
-    h_a[i] = i;
-    h_b[i] = i + 1;
-    h_result[i] = 0; 
+    h_a[i]      = i;
+    h_b[i]      = i + 1;
+    h_result[i] = 0;
   }
 
   onDevice(h_a, h_b, h_result);
 
-  //check the results
   for (int i = 0; i < ARRAY_SIZE; ++i) {
     assert(h_a[i] + h_b[i] == h_result[i]);
-    printf("h_a=%d, h_b=%d, h_result=%d\n", h_a[i], h_b[i], h_result[i]);
+    printf("ha=%d, hb=%d, h_result=%d\n", h_a[i], h_b[i], h_result[i]);
   }
 
   printf("-: successful execution :-\n");
@@ -64,9 +65,7 @@ void onHost() {
 }
 
 
-
 int main() {
-
   onHost();
   return 0;
 }
--- Makefile
+++ Makefile
@@ -3,4 +3,4 @@
 	@nvcc 01simple_kernel.cu -o exe1
 	@nvcc 01simple_kernel2.cu -o exe1_2
 	@nvcc 02add_kernel.cu -o exe2
-		
+	@nvcc 03add_kernel.cu -o exe3	
